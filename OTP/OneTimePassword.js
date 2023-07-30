5d296877431c5dc1eaf544d2442ad02b8fd482de
Mani kandan <manicdon7@example.com>
2023-07-30 22:10:35 +0530

    OTP sender

---
+++ OTP/OneTimePassword.js
@@ -0,0 +1,25 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Set environment variables for your credentials
+// Read more at http://twil.io/secure
+const accountSid = "ACea7a42da70fa37cdd997a2c8674befe8";
+const authToken = "19739a1f05bf24db1fe12ca39a49eef0";
+const verifySid = "VA285fbdc03b1f10d2b25613567eab05c8";
+const client = require("twilio")(accountSid, authToken);
+
+client.verify.v2
+  .services(verifySid)
+  .verifications.create({ to: "+919962023668", channel: "sms" })
+  .then((verification) => console.log(verification.status))
+  .then(() => {
+    const readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    readline.question("Please enter the OTP:", (otpCode) => {
+      client.verify.v2
+        .services(verifySid)
+        .verificationChecks.create({ to: "+919962023668", code: otpCode })
+        .then((verification_check) => console.log(verification_check.status))
+        .then(() => readline.close());
+    });
+  });
